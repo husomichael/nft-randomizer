ebdfb525c23e5a33dd3f18d0bdb7818256a1ee6a
Michael Huso <husomichael@gmail.com>
2022-01-03 15:07:59 -0600

    randomInt function generates random number 1-100 on random.router.js

--- server/routes/random.router.js
+++ server/routes/random.router.js
@@ -2,12 +2,17 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+//Generate random number between 1 and 100.
+function randomInt(){
+    return Math.floor(Math.random() * 100) + 1
+}
+
 //POST details for randomization.
 router.post('/', (req, res) => {
     console.log('random post', req.body);
-
-    
-
+    for(let i = 0; i<500; i++){
+        console.log(randomInt());
+    }
 });
 
 module.exports = router;
