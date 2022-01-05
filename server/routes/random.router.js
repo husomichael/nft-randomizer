fb281096a8ef60f9579f89c349a3a00f58561b0d
Michael Huso <husomichael@gmail.com>
2022-01-05 16:06:24 -0600

    randomizer server code workinggit add .

--- server/routes/random.router.js
+++ server/routes/random.router.js
@@ -5,14 +5,42 @@ const router = express.Router();
 //Generate random number between 1 and 100.
 function randomInt(){
     return Math.floor(Math.random() * 100) + 1
-}
+};
 
 //POST details for randomization.
 router.post('/', (req, res) => {
-    console.log('random post', req.body);
-    for(let i = 0; i<500; i++){
-        console.log(randomInt());
-    }
-});
+    let project = req.body;
+    let returnArray = [];
+    let rowArray = [];
+    let rarityRange = 0;
+    let roll = 0;
+    let attributesForLayer = [];
+    for(let i = 0; i<project.number; i++){
+        for (let layer of project.layers){
+            rarityRange = 0;
+            attributesForLayer = [];
+            roll = randomInt();
+
+            for(let attribute of project.attributes){
+                if(attribute.layer_id == layer.id){
+                    attributesForLayer.push(attribute);
+                };
+            };//end for of all attributes filter for layer loop
+
+            for(let selectedAttribute of attributesForLayer){
+                if(roll > rarityRange && roll <= (rarityRange + selectedAttribute.rarity_value)){
+                    rowArray.push(selectedAttribute.attribute_name);
+                    rarityRange+= selectedAttribute.rarity_value
+                }else{
+                    rarityRange += selectedAttribute.rarity_value;
+                };
+            };//end for of selectedAttribute loop.
+
+        };//end for of layers loop
+        returnArray.push(rowArray);
+        rowArray = [];
+    };//End mint number for loop
+    console.log('returnArray:', returnArray);
+}); // End /random POST
 
 module.exports = router;
